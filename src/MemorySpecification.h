tukl-msd/DRAMPower

commit da8778b61ae671855c84350af2217b5fdd4472d0

[#12] Added MemoryType class to replace enum.
Reduced number of elaborate comparisons with the memory type. Added little class to represent the type instead of an enum, to give it some intelligence.

src/MemorySpecification.h
@@ -39,50 +39,84 @@
 #define TOOLS_MEMORY_SPECIFICATION_H
 
 #include <cassert>
+#include <string>
 
 #include "MemArchitectureSpec.h"
 #include "MemTimingSpec.h"
 #include "MemPowerSpec.h"
 #include "Parametrisable.h"
 
+
 namespace Data {
-class MemorySpecification : public virtual Parametrisable {
+// Supported memory types
+class MemoryType {
  public:
-  // Supported memory types
-  enum MemoryType {
-    DDR2,
+  enum MemoryType_t {
+    DDR2 = 0,
     DDR3,
     DDR4,
     LPDDR,
     LPDDR2,
     LPDDR3,
-    WIDEIO_SDR
+    WIDEIO_SDR,
+    MEMORY_TYPE_INVALID
   };
 
-  static const unsigned int nMemoryTypes = 7;
-
-  static std::string* getMemoryTypeStrings()
+  MemoryType(MemoryType_t _val) :
+    val(_val)
   {
-    static std::string type_map[nMemoryTypes] = { "DDR2",   "DDR3",   "DDR4", "LPDDR",
-                                                  "LPDDR2", "LPDDR3", "WIDEIO_SDR" };
-
-    return type_map;
   }
 
-  // To identify memory type from name
-  static MemoryType getMemoryTypeFromName(const std::string& name)
+  MemoryType() :
+    val(MEMORY_TYPE_INVALID)
   {
-    std::string* typeStrings = getMemoryTypeStrings();
+  }
 
-    for (size_t typeId = 0; typeId < nMemoryTypes; typeId++) {
-      if (typeStrings[typeId] == name) {
-        MemoryType memoryType = static_cast<MemoryType>(typeId);
-        return memoryType;
-      }
+  MemoryType(const std::string& _val) :
+    val(MEMORY_TYPE_INVALID)
+  {
+    if (_val == "DDR2") {
+      val == DDR2;
+    } else if (_val == "DDR3") {
+      val == DDR3;
+    } else if (_val == "DDR4") {
+      val == DDR4;
+    } else if (_val == "LPDDR") {
+      val == LPDDR;
+    } else if (_val == "LPDDR2") {
+      val == LPDDR2;
+    } else if (_val == "LPDDR3") {
+      val == LPDDR3;
+    } else if (_val == "WIDEIO_SDR") {
+      val == WIDEIO_SDR;
     }
-    assert(false); // Unknown name.
   }
 
+  bool isLPDDRFamily() const
+  {
+    return val == LPDDR ||
+           val == LPDDR2 ||
+           val == LPDDR3 ||
+           val == WIDEIO_SDR;
+  }
+
+  bool isDDRFamily() const
+  {
+    return val == DDR2 ||
+           val == DDR2 ||
+           val == DDR4;
+  }
+
+  operator MemoryType_t() const {
+    return val;
+  }
+
+ private:
+  MemoryType_t val;
+};
+
+class MemorySpecification : public virtual Parametrisable {
+ public:
   std::string id;
   MemoryType  memoryType;
 
@@ -94,5 +128,5 @@ class MemorySpecification : public virtual Parametrisable {
 
   static MemorySpecification getMemSpecFromXML(const std::string& id);
 };
-}
+}  // namespace Data
 #endif // ifndef TOOLS_MEMORY_SPECIFICATION_H

src/CmdScheduler.cc
@@ -269,13 +269,13 @@ void cmdScheduler::analyticalScheduling(MemorySpecification memSpec)
 
     for (int i = 0; i < BI; i++) {
       for (int k = 0; k < BC; k++) {
-        if (memSpec.memoryType == MemorySpecification::DDR4) {
+        if (memSpec.memoryType == MemoryType::DDR4) {
           bankGroupPointer = PhysicalAddress.bankGroupAddr;
         }
 
         for (int j = 0; j < BGI; j++) {
           bankGroupSwitch = false;
-          if (memSpec.memoryType == MemorySpecification::DDR4) {
+          if (memSpec.memoryType == MemoryType::DDR4) {
             if (bankGroupPointer != bankGroupAddr) {
               bankGroupSwitch = true;
             }
@@ -305,8 +305,7 @@ void cmdScheduler::analyticalScheduling(MemorySpecification memSpec)
                                             ACT[bankaccess - 4].time +
                                             static_cast<int>(memTimingSpec.FAW));
 
-            if (memSpec.memoryType ==
-                memSpec.getMemoryTypeFromName("WIDEIO_SDR")) {
+            if (memSpec.memoryType == MemoryType::WIDEIO_SDR) {
               cmd.time = max(max(ACT[bankaccess - 1].time + tRRD_init,
                                  PRE[cmd.bank].time + static_cast<int>(memTimingSpec.RP)),
                              ACT[bankaccess - 2].time +
@@ -465,10 +464,7 @@ void cmdScheduler::pdScheduling(double endTime, double timer,
     else
       cmd.time = cmd.time + pdTime;
 
-    if ((memSpec.memoryType == memSpec.getMemoryTypeFromName("LPDDR"))
-        || (memSpec.memoryType == memSpec.getMemoryTypeFromName("LPDDR2"))
-        || (memSpec.memoryType == memSpec.getMemoryTypeFromName("LPDDR3"))
-        || (memSpec.memoryType == memSpec.getMemoryTypeFromName("WIDEIO_SDR")))
+    if (memSpec.memoryType.isLPDDRFamily())
       startTime = cmd.time + memTimingSpec.XP;
     else
       startTime = cmd.time + memTimingSpec.XPDLL - memTimingSpec.RCD;
@@ -483,10 +479,7 @@ void cmdScheduler::pdScheduling(double endTime, double timer,
     cmd.name      = "SREX";
     cmd.time      = cmd.time + pdTime;
 
-    if ((memSpec.memoryType == memSpec.getMemoryTypeFromName("LPDDR"))
-        || (memSpec.memoryType == memSpec.getMemoryTypeFromName("LPDDR2"))
-        || (memSpec.memoryType == memSpec.getMemoryTypeFromName("LPDDR3"))
-        || (memSpec.memoryType == memSpec.getMemoryTypeFromName("WIDEIO_SDR")))
+    if (memSpec.memoryType.isLPDDRFamily())
       startTime = cmd.time + memTimingSpec.XS;
     else
       startTime = cmd.time + memTimingSpec.XSDLL - memTimingSpec.RCD;
@@ -505,38 +498,38 @@ int cmdScheduler::getRWTP(int transType, MemorySpecification memSpec)
 
   if (transType == READ) {
     switch (memSpec.memoryType) {
-    case MemorySpecification::LPDDR:
-    case MemorySpecification::WIDEIO_SDR:
+    case MemoryType::LPDDR:
+    case MemoryType::WIDEIO_SDR:
       tRWTP_init = memArchSpec.burstLength / memArchSpec.dataRate;
       break;
 
-    case MemorySpecification::LPDDR2:
-    case MemorySpecification::LPDDR3:
+    case MemoryType::LPDDR2:
+    case MemoryType::LPDDR3:
       tRWTP_init = memArchSpec.burstLength / memArchSpec.dataRate +
                    max(0, static_cast<int>(memTimingSpec.RTP - 2));
       break;
 
-    case MemorySpecification::DDR2:
+    case MemoryType::DDR2:
       tRWTP_init = memTimingSpec.AL + memArchSpec.burstLength /
                    memArchSpec.dataRate +
                    max(static_cast<int>(memTimingSpec.RTP), 2) - 2;
       break;
 
-    case MemorySpecification::DDR3:
-    case MemorySpecification::DDR4:
+    case MemoryType::DDR3:
+    case MemoryType::DDR4:
       tRWTP_init = memTimingSpec.RTP;
       break;
     } // switch
   } else if (transType == WRITE)    {
-    if (memSpec.memoryType == MemorySpecification::WIDEIO_SDR) {
+    if (memSpec.memoryType == MemoryType::WIDEIO_SDR) {
       tRWTP_init = memTimingSpec.WL + memArchSpec.burstLength /
                    memArchSpec.dataRate - 1 + memSpec.memTimingSpec.WR;
     } else   {
       tRWTP_init = memTimingSpec.WL + memArchSpec.burstLength /
                    memArchSpec.dataRate + memSpec.memTimingSpec.WR;
     }
-    if ((memSpec.memoryType == MemorySpecification::LPDDR2) ||
-        (memSpec.memoryType == MemorySpecification::LPDDR3)) {
+    if ((memSpec.memoryType == MemoryType::LPDDR2) ||
+        (memSpec.memoryType == MemoryType::LPDDR3)) {
       tRWTP_init = tRWTP_init + 1;
     }
   }
@@ -554,7 +547,7 @@ void cmdScheduler::getTimingConstraints(bool BGSwitch, MemorySpecification memSp
   MemTimingSpec& memTimingSpec     = memSpec.memTimingSpec;
   MemArchitectureSpec& memArchSpec = memSpec.memArchSpec;
 
-  if (memSpec.memoryType != MemorySpecification::DDR4) {
+  if (memSpec.memoryType != MemoryType::DDR4) {
     tRRD_init = memTimingSpec.RRD;
     if (PreType == CurrentType) {
       tSwitch_init = memTimingSpec.CCD;
@@ -562,7 +555,7 @@ void cmdScheduler::getTimingConstraints(bool BGSwitch, MemorySpecification memSp
     }
 
     if ((PreType == WRITE) && (CurrentType == READ)) {
-      if (memSpec.memoryType == MemorySpecification::WIDEIO_SDR) {
+      if (memSpec.memoryType == MemoryType::WIDEIO_SDR) {
         tSwitch_init = memTimingSpec.WL + memArchSpec.burstLength /
                        memArchSpec.dataRate - 1 + memTimingSpec.WTR;
       } else   {
@@ -570,14 +563,14 @@ void cmdScheduler::getTimingConstraints(bool BGSwitch, MemorySpecification memSp
                        memArchSpec.dataRate + memTimingSpec.WTR;
       }
 
-      if ((memSpec.memoryType == MemorySpecification::LPDDR2) ||
-          (memSpec.memoryType == MemorySpecification::LPDDR3)) {
+      if ((memSpec.memoryType == MemoryType::LPDDR2) ||
+          (memSpec.memoryType == MemoryType::LPDDR3)) {
         tSwitch_init = tSwitch_init + 1;
       }
     }
   }
 
-  if (memSpec.memoryType == MemorySpecification::DDR4) {
+  if (memSpec.memoryType == MemoryType::DDR4) {
     if (BGSwitch) {
       tCCD_init = memTimingSpec.CCD_S;
       tRRD_init = memTimingSpec.RRD_S;
@@ -614,7 +607,7 @@ cmdScheduler::physicalAddr cmdScheduler::memoryMap(trans               Trans,
   DecLogic = Trans.logicalAddress;
 
   // row-bank-column-BI-BC-BGI-BL
-  if ((BGI > 1) && (memSpec.memoryType == MemorySpecification::DDR4)) {
+  if ((BGI > 1) && (memSpec.memoryType == MemoryType::DDR4)) {
     unsigned colBits   = static_cast<unsigned>(log2(nColumns));
     unsigned bankShift = static_cast<unsigned>(colBits + ((BI > 1) ? log2(BI) : 0)
                                                + ((BGI > 1) ? log2(BGI) : 0));

src/CommandAnalysis.cc
@@ -495,10 +495,7 @@ void CommandAnalysis::evaluate(const MemorySpecification& memSpec,
                                memSpec.memTimingSpec.XP - memSpec.memTimingSpec.RCD);
       } else if (mem_state == CommandAnalysis::MS_PDN_S_ACT) {
         s_act_pdcycles += max(zero, timestamp - pdn_cycle);
-        if ((memSpec.memoryType == memSpec.getMemoryTypeFromName("LPDDR"))
-            || (memSpec.memoryType == memSpec.getMemoryTypeFromName("LPDDR2"))
-            || (memSpec.memoryType == memSpec.getMemoryTypeFromName("LPDDR3"))
-            || (memSpec.memoryType == memSpec.getMemoryTypeFromName("WIDEIO_SDR"))) {
+        if (memSpec.memoryType.isLPDDRFamily()) {
           pup_act_cycles  += memSpec.memTimingSpec.XP;
           latest_act_cycle = max(timestamp, timestamp +
                                  memSpec.memTimingSpec.XP - memSpec.memTimingSpec.RCD);
@@ -530,10 +527,7 @@ void CommandAnalysis::evaluate(const MemorySpecification& memSpec,
                                memSpec.memTimingSpec.XP - memSpec.memTimingSpec.RP);
       } else if (mem_state == CommandAnalysis::MS_PDN_S_PRE) {
         s_pre_pdcycles += max(zero, timestamp - pdn_cycle);
-        if ((memSpec.memoryType == memSpec.getMemoryTypeFromName("LPDDR"))
-            || (memSpec.memoryType == memSpec.getMemoryTypeFromName("LPDDR2"))
-            || (memSpec.memoryType == memSpec.getMemoryTypeFromName("LPDDR3"))
-            || (memSpec.memoryType == memSpec.getMemoryTypeFromName("WIDEIO_SDR"))) {
+        if (memSpec.memoryType.isLPDDRFamily()) {
           pup_pre_cycles  += memSpec.memTimingSpec.XP;
           latest_pre_cycle = max(timestamp, timestamp +
                                  memSpec.memTimingSpec.XP - memSpec.memTimingSpec.RP);
@@ -582,10 +576,7 @@ void CommandAnalysis::evaluate(const MemorySpecification& memSpec,
                                memSpec.memTimingSpec.RP;
         sref_ref_pre_cycles += memSpec.memTimingSpec.RP;
         last_pre_cycle       = timestamp;
-        if ((memSpec.memoryType == memSpec.getMemoryTypeFromName("LPDDR"))
-            || (memSpec.memoryType == memSpec.getMemoryTypeFromName("LPDDR2"))
-            || (memSpec.memoryType == memSpec.getMemoryTypeFromName("LPDDR3"))
-            || (memSpec.memoryType == memSpec.getMemoryTypeFromName("WIDEIO_SDR"))) {
+        if (memSpec.memoryType.isLPDDRFamily()) {
           spup_cycles     += memSpec.memTimingSpec.XS;
           latest_pre_cycle = max(timestamp, timestamp +
                                  memSpec.memTimingSpec.XS - memSpec.memTimingSpec.RP);
@@ -608,11 +599,7 @@ void CommandAnalysis::evaluate(const MemorySpecification& memSpec,
           sref_ref_pre_cycles += sref_pre;
           spup_ref_pre_cycles += spup_pre;
           last_pre_cycle       = timestamp + spup_pre;
-          if ((memSpec.memoryType == memSpec.getMemoryTypeFromName("LPDDR"))
-              || (memSpec.memoryType == memSpec.getMemoryTypeFromName("LPDDR2"))
-              || (memSpec.memoryType == memSpec.getMemoryTypeFromName("LPDDR3"))
-              || (memSpec.memoryType ==
-                  memSpec.getMemoryTypeFromName("WIDEIO_SDR"))) {
+          if (memSpec.memoryType.isLPDDRFamily()) {
             spup_cycles     += memSpec.memTimingSpec.XS - spup_pre;
             latest_pre_cycle = max(timestamp, timestamp +
                                    memSpec.memTimingSpec.XS - spup_pre -
@@ -629,11 +616,7 @@ void CommandAnalysis::evaluate(const MemorySpecification& memSpec,
           spup_ref_act_cycles += spup_act;
           spup_ref_pre_cycles += memSpec.memTimingSpec.RP;
           last_pre_cycle       = timestamp + spup_act + memSpec.memTimingSpec.RP;
-          if ((memSpec.memoryType == memSpec.getMemoryTypeFromName("LPDDR"))
-              || (memSpec.memoryType == memSpec.getMemoryTypeFromName("LPDDR2"))
-              || (memSpec.memoryType == memSpec.getMemoryTypeFromName("LPDDR3"))
-              || (memSpec.memoryType ==
-                  memSpec.getMemoryTypeFromName("WIDEIO_SDR"))) {
+          if (memSpec.memoryType.isLPDDRFamily()) {
             spup_cycles     += memSpec.memTimingSpec.XS - spup_act -
                                memSpec.memTimingSpec.RP;
             latest_pre_cycle = max(timestamp, timestamp +

src/MemCommand.cc
@@ -95,7 +95,7 @@ int MemCommand::getPrechargeOffset(const MemorySpecification& memSpec,
 
   // Read with auto-precharge
   if (type == MemCommand::RDA) {
-    if (memSpec.memoryType == MemorySpecification::DDR2)
+    if (memSpec.memoryType == MemoryType::DDR2)
       precharge_offset = AL + BL / dataRate + max(RTP, 2) - 2;
     else
       precharge_offset = RTP;

src/MemoryPowerModel.cc
@@ -130,7 +130,7 @@ void MemoryPowerModel::power_calc(MemorySpecification memSpec,
                             + energy.read_oterm_energy + energy.write_oterm_energy;
   }
 
-  MemorySpecification::MemoryType memoryType;
+  MemoryType memoryType;
   memoryType   = memSpec.memoryType;
 
   total_cycles = timings.actcycles + timings.precycles +
@@ -226,10 +226,7 @@ void MemoryPowerModel::power_calc(MemorySpecification memSpec,
 
   // similar equations as before to support multiple voltage domains in LPDDR2
   // and WIDEIO memories
-  if ((memoryType == MemorySpecification::getMemoryTypeFromName("LPDDR2")) ||
-      (memoryType == MemorySpecification::getMemoryTypeFromName("LPDDR3")) ||
-      (memoryType == MemorySpecification::getMemoryTypeFromName("DDR4")) ||
-      (memoryType == MemorySpecification::getMemoryTypeFromName("WIDEIO_SDR"))) {
+  if (memoryType.isLPDDRFamily()) {
     energy.act_energy += timings.numberofacts * engy_act(memPowerSpec.idd3n2,
                                                          memPowerSpec.idd02, memPowerSpec.vdd2, memTimingSpec.RAS,
                                                          memTimingSpec.clkPeriod);
@@ -395,16 +392,11 @@ void MemoryPowerModel::power_print(MemorySpecification memSpec, int term)
   if (term) {
     cout << "RD I/O Energy: " << energy.read_io_energy << " pJ" << endl;
     // No Termination for LPDDR/2/3 and DDR memories
-    if ((memSpec.memoryType == MemorySpecification::getMemoryTypeFromName("DDR2"))
-        || (memSpec.memoryType == MemorySpecification::getMemoryTypeFromName("DDR3"))
-        || (memSpec.memoryType == MemorySpecification::getMemoryTypeFromName("DDR4"))) {
+    if (memSpec.memoryType.isDDRFamily()) {
       cout << "WR Termination Energy: " << energy.write_term_energy << " pJ" << endl;
     }
 
-    if ((memArchSpec.nbrOfRanks > 1) && ((memSpec.memoryType ==
-                                          MemorySpecification::getMemoryTypeFromName("DDR2")) || (memSpec.memoryType ==
-                                                                                                  MemorySpecification::getMemoryTypeFromName("DDR3")) || (memSpec.memoryType ==
-                                                                                                                                                          MemorySpecification::getMemoryTypeFromName("DDR4")))) {
+    if ((memArchSpec.nbrOfRanks > 1) && memSpec.memoryType.isDDRFamily()) {
       cout << "RD Termination Energy (Idle rank): " << energy.read_oterm_energy
            << " pJ" << endl;
       cout << "WR Termination Energy (Idle rank): " << energy.write_oterm_energy
@@ -587,10 +579,10 @@ double MemoryPowerModel::engy_sref(double idd6, double idd3n, double idd5,
 // Absolute power measures are obtained from Micron Power Calculator (mentioned in mW)
 void MemoryPowerModel::io_term_power(MemorySpecification memSpec)
 {
-  MemTimingSpec& memTimingSpec                = memSpec.memTimingSpec;
-  MemArchitectureSpec& memArchSpec            = memSpec.memArchSpec;
-  MemPowerSpec&  memPowerSpec                 = memSpec.memPowerSpec;
-  MemorySpecification::MemoryType& memoryType = memSpec.memoryType;
+  MemTimingSpec& memTimingSpec     = memSpec.memTimingSpec;
+  MemArchitectureSpec& memArchSpec = memSpec.memArchSpec;
+  MemPowerSpec&  memPowerSpec      = memSpec.memPowerSpec;
+  MemoryType& memoryType           = memSpec.memoryType;
 
   // For LPDDR/2/3 memories - IO Power depends on DRAM clock frequency
   // No ODT (Termination) in LPDDR/2/3 and DDR memories
@@ -602,7 +594,7 @@ void MemoryPowerModel::io_term_power(MemorySpecification memSpec)
   double LPDDR3_Cap = 0.0000000018;
 
   // Conservative estimates based on Micron DDR2 Power Calculator
-  if (memoryType == MemorySpecification::getMemoryTypeFromName("DDR2")) {
+  if (memoryType == MemoryType::DDR2) {
     power.IO_power     = 1.5;    // in mW
     power.WR_ODT_power = 8.2;    // in mW
     if (memArchSpec.nbrOfRanks > 1) {
@@ -611,7 +603,7 @@ void MemoryPowerModel::io_term_power(MemorySpecification memSpec)
     }
   }
   // Conservative estimates based on Micron DDR3 Power Calculator
-  else if (memoryType == MemorySpecification::getMemoryTypeFromName("DDR3")) {
+  else if (memoryType == MemoryType::DDR3) {
     power.IO_power     = 4.6;    // in mW
     power.WR_ODT_power = 21.2;   // in mW
     if (memArchSpec.nbrOfRanks > 1) {
@@ -621,7 +613,7 @@ void MemoryPowerModel::io_term_power(MemorySpecification memSpec)
   }
   // Conservative estimates based on Micron DDR3 Power Calculator
   // using available termination resistance values from Micron DDR4 Datasheets
-  else if (memoryType == MemorySpecification::getMemoryTypeFromName("DDR4")) {
+  else if (memoryType == MemoryType::DDR4) {
     power.IO_power     = 3.7;    // in mW
     power.WR_ODT_power = 17.0;   // in mW
     if (memArchSpec.nbrOfRanks > 1) {
@@ -631,13 +623,14 @@ void MemoryPowerModel::io_term_power(MemorySpecification memSpec)
   }
   // LPDDR/2/3 and DDR memories only have IO Power (no ODT)
   // Conservative estimates based on Micron Mobile LPDDR2 Power Calculator
-  else if (memoryType == MemorySpecification::getMemoryTypeFromName("LPDDR")) {
+  else if (memoryType == MemoryType::LPDDR) {
     power.IO_power = LPDDR_Cap * power.IO_power;
-  } else if (memoryType == MemorySpecification::getMemoryTypeFromName("LPDDR2"))    {
+  } else if (memoryType == MemoryType::LPDDR2)    {
     power.IO_power = LPDDR2_Cap * power.IO_power;
-  } else if (memoryType == MemorySpecification::getMemoryTypeFromName("LPDDR3"))    {
+  } else if (memoryType == MemoryType::LPDDR3)    {
     power.IO_power = LPDDR3_Cap * power.IO_power;
-  } else if (memoryType == MemorySpecification::getMemoryTypeFromName("DDR"))    {
-    power.IO_power = 6.88; // in mW
   }
+  // } else if (memoryType == MemorySpecification::getMemoryTypeFromName("DDR"))    {
+  //   power.IO_power = 6.88; // in mW
+  // }
 } // MemoryPowerModel::io_term_power

src/MemorySpecification.cc
@@ -43,8 +43,6 @@ using namespace Data;
 // Set variable values from XML
 void MemorySpecification::processParameters()
 {
-  setVarFromParam(&id,               "memoryId");
-  string memoryTypeString;
-  setVarFromParam(&memoryTypeString, "memoryType");
-  memoryType = getMemoryTypeFromName(memoryTypeString);
+  setVarFromParam(&id,"memoryId");
+  setVarFromParam(&memoryType, "memoryType");
 }

commit 8a661c84c66681e4d0c2d70db676e12761648cbf

[#12] Fixed comparison bug introduced in da8778b
da8778b61ae671855c84350af2217b5fdd4472d0

src/MemorySpecification.h
@@ -76,20 +76,21 @@ class MemoryType {
     val(MEMORY_TYPE_INVALID)
   {
     if (_val == "DDR2") {
-      val == DDR2;
+      val = DDR2;
     } else if (_val == "DDR3") {
-      val == DDR3;
+      val = DDR3;
     } else if (_val == "DDR4") {
-      val == DDR4;
+      val = DDR4;
     } else if (_val == "LPDDR") {
-      val == LPDDR;
+      val = LPDDR;
     } else if (_val == "LPDDR2") {
-      val == LPDDR2;
+      val = LPDDR2;
     } else if (_val == "LPDDR3") {
-      val == LPDDR3;
+      val = LPDDR3;
     } else if (_val == "WIDEIO_SDR") {
-      val == WIDEIO_SDR;
+      val = WIDEIO_SDR;
     }
+    assert("Unknown memory type." && val != MEMORY_TYPE_INVALID);
   }
 
   bool isLPDDRFamily() const