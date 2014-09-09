tukl-msd/DRAMPower

commit 128f20e02583c75e69ecb8850e6db65e85986f37

Fixed bug in io/termination energy calculation.
Not it takes the clock period into account. Updated test accordingly.

src/MemoryPowerModel.cc
@@ -86,37 +86,47 @@ void MemoryPowerModel::power_calc(MemorySpecification memSpec,
   energy.write_oterm_energy  = 0.0;
   energy.io_term_energy      = 0.0;
 
+  // How long a single burst takes, measured in command-clock cycles.
+  int64_t burstCc = memArchSpec.burstLength / memArchSpec.dataRate;
+
   // IO and Termination Power measures are included, if required.
   if (term) {
     io_term_power(memSpec);
 
-    // Read IO power is consumed by each DQ (data) and DQS (data strobe) pin
-    // Width represents data pins
+    // memArchSpec.width represents the number of data (dq) pins.
     // 1 DQS pin is associated with every data byte
-    energy.read_io_energy = (power.IO_power * static_cast<double>(counters.numberofreads) * memArchSpec.burstLength
-                             * (memArchSpec.width + memArchSpec.width / 8));
+    int64_t dqPlusDqsBits = memArchSpec.width + memArchSpec.width / 8;
+    // 1 DQS and 1 DM pin is associated with every data byte
+    int64_t dqPlusDqsPlusMaskBits = memArchSpec.width + memArchSpec.width / 8 + memArchSpec.width / 8;
+    // Size of one clock period for the data bus.
+    double ddrPeriod = memTimingSpec.clkPeriod * memArchSpec.dataRate;
+
+    // Read IO power is consumed by each DQ (data) and DQS (data strobe) pin
+    energy.read_io_energy = calcIoTermEnergy(counters.numberofreads * burstCc,
+                                             ddrPeriod,
+                                             power.IO_power,
+                                             dqPlusDqsBits);
 
     // Write ODT power is consumed by each DQ (data), DQS (data strobe) and DM
-    // (data mask) pin.
-    // Width represents data pins
-    // 1 DQS and 1 DM pin is associated with every data byte
-    energy.write_term_energy = (power.WR_ODT_power * static_cast<double>(counters.numberofwrites) *
-                                memArchSpec.burstLength * (memArchSpec.width + memArchSpec.width / 4));
+    energy.write_term_energy = calcIoTermEnergy(counters.numberofwrites * burstCc,
+                                                ddrPeriod,
+                                                power.WR_ODT_power,
+                                                dqPlusDqsPlusMaskBits);
 
     if (memArchSpec.nbrOfRanks > 1) {
       // Termination power consumed in the idle rank during reads on the active
       // rank by each DQ (data) and DQS (data strobe) pin.
-      // Width represents data pins
-      // 1 DQS pin is associated with every data byte
-      energy.read_oterm_energy = (power.TermRD_power * static_cast<double>(counters.numberofreads) *
-                                  memArchSpec.burstLength * (memArchSpec.width + memArchSpec.width / 8));
+      energy.read_oterm_energy = calcIoTermEnergy(counters.numberofreads * burstCc,
+                                                  ddrPeriod,
+                                                  power.TermRD_power,
+                                                  dqPlusDqsBits);
 
       // Termination power consumed in the idle rank during writes on the active
       // rank by each DQ (data), DQS (data strobe) and DM (data mask) pin.
-      // Width represents data pins
-      // 1 DQS and 1 DM pin is associated with every data byte
-      energy.write_oterm_energy = (power.TermWR_power * static_cast<double>(counters.numberofwrites) *
-                                   memArchSpec.burstLength * (memArchSpec.width + memArchSpec.width / 4));
+      energy.write_oterm_energy = calcIoTermEnergy(counters.numberofwrites * burstCc,
+                                                   ddrPeriod,
+                                                   power.TermWR_power,
+                                                   dqPlusDqsPlusMaskBits);
     }
 
     // Sum of all IO and termination energy
@@ -585,3 +595,9 @@ void MemoryPowerModel::io_term_power(MemorySpecification memSpec)
     power.IO_power = memPowerSpec.capacitance * 0.5 * pow(memPowerSpec.vdd2, 2.0) * memTimingSpec.clkMhz * 1000000;
   }
 } // MemoryPowerModel::io_term_power
+
+
+double MemoryPowerModel::calcIoTermEnergy(int64_t cycles, double period, double power, int64_t numBits)
+{
+  return static_cast<double>(cycles) * period * power * static_cast<double>(numBits);
+}

test/reference/test_LPDDR2_1066_termination_matches_reference.out
@@ -1,7 +1,5 @@
 * Parsing ../DRAMPower/memspecs/MICRON_2Gb_LPDDR2-1066-S4_16bit_A.xml
-* Analysis start time: Thu Sep  4 13:34:47 2014
 * Analyzing the input trace
-* Power Computation Start time: Thu Sep  4 13:34:47 2014
 * Trace Details:
 Number of Activates: 4000
 Number of Reads: 8000
@@ -42,7 +40,7 @@ ACT Cmd Energy: 14333358.35 pJ
 PRE Cmd Energy: 6196322.70 pJ
 RD Cmd Energy: 14531482.18 pJ
 WR Cmd Energy: 0.00 pJ
-RD I/O Energy: 1105228.80 pJ
+RD I/O Energy: 2073600.00 pJ
 ACT Stdby Energy: 2966241.95 pJ
   Active Idle Energy: 0.00 pJ
   Active Power-Up Energy: 0.00 pJ
@@ -63,8 +61,7 @@ Total Power-Down Energy: 0.00 pJ
 Auto-Refresh Energy: 379386.87 pJ
 Self-Refresh Energy: 0.00 pJ
 ----------------------------------------
-Total Trace Energy: 39539822.68 pJ
-Average Power: 502.57 mW
+Total Trace Energy: 40508193.88 pJ
+Average Power: 514.88 mW
 ----------------------------------------
-* Power Computation End time: Thu Sep  4 13:34:47 2014
-* Total Simulation time: 0.06 seconds
+

src/MemArchitectureSpec.cc
@@ -37,6 +37,8 @@
 
 #include "MemArchitectureSpec.h"
 
+#include <cassert>
+
 using namespace Data;
 
 void MemArchitectureSpec::processParameters()
@@ -50,6 +52,7 @@ void MemArchitectureSpec::processParameters()
   nbrOfColumns    = getParamValWithDefault("nbrOfColumns", 1);
   nbrOfRows       = getParamValWithDefault("nbrOfRows", 1);
   width           = getParamValWithDefault("width", 1);
+  assert("memory width should be a multiple of 8" && (width % 8) == 0);
   dll             = getParamValWithDefault("dll", false);
   twoVoltageDomains = getParamValWithDefault("twoVoltageDomains", false);
   termination       = getParamValWithDefault("termination", false);

src/MemoryPowerModel.h
@@ -226,6 +226,9 @@ class MemoryPowerModel {
 
   Energy energy;
   Power  power;
+
+ private:
+  double calcIoTermEnergy(int64_t cycles, double period, double power, int64_t numBits);
 };
 }
 #endif // ifndef MEMORY_POWER_MODEL_H

test/test.py
@@ -50,6 +50,7 @@ class TestUsingBuildResult(unittest.TestCase):
 
 class TestOutput(TestUsingBuildResult):
     def run_and_compare_to_reference(self, cmd, referenceFile):
+        self.maxDiff = None  # Show full diff on error.
         with open(self.tempFileName, 'w') as f:
             subprocess.call(cmd, stdout = f)
 

commit 9e2fe2c54898daea2ab5ea2c128693db5d2bc9fb

Fix for bug introduced in 128f20e025.

src/MemoryPowerModel.cc
@@ -39,6 +39,9 @@
 
 #include <cmath>  // For pow
 
+#include <stdint.h>
+
+
 using namespace std;
 using namespace Data;
 
@@ -87,7 +90,7 @@ void MemoryPowerModel::power_calc(MemorySpecification memSpec,
   energy.io_term_energy      = 0.0;
 
   // How long a single burst takes, measured in command-clock cycles.
-  int64_t burstCc = memArchSpec.burstLength / memArchSpec.dataRate;
+  // int64_t burstCc = memArchSpec.burstLength / memArchSpec.dataRate;
 
   // IO and Termination Power measures are included, if required.
   if (term) {
@@ -99,16 +102,16 @@ void MemoryPowerModel::power_calc(MemorySpecification memSpec,
     // 1 DQS and 1 DM pin is associated with every data byte
     int64_t dqPlusDqsPlusMaskBits = memArchSpec.width + memArchSpec.width / 8 + memArchSpec.width / 8;
     // Size of one clock period for the data bus.
-    double ddrPeriod = memTimingSpec.clkPeriod * memArchSpec.dataRate;
+    double ddrPeriod = memTimingSpec.clkPeriod / memArchSpec.dataRate;
 
     // Read IO power is consumed by each DQ (data) and DQS (data strobe) pin
-    energy.read_io_energy = calcIoTermEnergy(counters.numberofreads * burstCc,
+    energy.read_io_energy = calcIoTermEnergy(counters.numberofreads * memArchSpec.burstLength,
                                              ddrPeriod,
                                              power.IO_power,
                                              dqPlusDqsBits);
 
     // Write ODT power is consumed by each DQ (data), DQS (data strobe) and DM
-    energy.write_term_energy = calcIoTermEnergy(counters.numberofwrites * burstCc,
+    energy.write_term_energy = calcIoTermEnergy(counters.numberofwrites * memArchSpec.burstLength,
                                                 ddrPeriod,
                                                 power.WR_ODT_power,
                                                 dqPlusDqsPlusMaskBits);
@@ -116,14 +119,14 @@ void MemoryPowerModel::power_calc(MemorySpecification memSpec,
     if (memArchSpec.nbrOfRanks > 1) {
       // Termination power consumed in the idle rank during reads on the active
       // rank by each DQ (data) and DQS (data strobe) pin.
-      energy.read_oterm_energy = calcIoTermEnergy(counters.numberofreads * burstCc,
+      energy.read_oterm_energy = calcIoTermEnergy(counters.numberofreads * memArchSpec.burstLength,
                                                   ddrPeriod,
                                                   power.TermRD_power,
                                                   dqPlusDqsBits);
 
       // Termination power consumed in the idle rank during writes on the active
       // rank by each DQ (data), DQS (data strobe) and DM (data mask) pin.
-      energy.write_oterm_energy = calcIoTermEnergy(counters.numberofwrites * burstCc,
+      energy.write_oterm_energy = calcIoTermEnergy(counters.numberofwrites * memArchSpec.burstLength,
                                                    ddrPeriod,
                                                    power.TermWR_power,
                                                    dqPlusDqsPlusMaskBits);

test/reference/test_LPDDR2_1066_termination_matches_reference.out
@@ -40,7 +40,7 @@ ACT Cmd Energy: 14333358.35 pJ
 PRE Cmd Energy: 6196322.70 pJ
 RD Cmd Energy: 14531482.18 pJ
 WR Cmd Energy: 0.00 pJ
-RD I/O Energy: 2073600.00 pJ
+RD I/O Energy: 1036800.00 pJ
 ACT Stdby Energy: 2966241.95 pJ
   Active Idle Energy: 0.00 pJ
   Active Power-Up Energy: 0.00 pJ
@@ -61,7 +61,7 @@ Total Power-Down Energy: 0.00 pJ
 Auto-Refresh Energy: 379386.87 pJ
 Self-Refresh Energy: 0.00 pJ
 ----------------------------------------
-Total Trace Energy: 40508193.88 pJ
-Average Power: 514.88 mW
+Total Trace Energy: 39471393.88 pJ
+Average Power: 501.70 mW
 ----------------------------------------
 