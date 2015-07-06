598128de7b72fe6851616b75dfb3163c21828c69
Sven Goossens <s.l.m.goossens@tue.nl>
2015-07-06 10:22:35 +0200

    PEP8 test.py

--- test/test.py
+++ test/test.py
@@ -8,9 +8,11 @@ import gzip
 
 devnull = None
 
+
 def inCoverageTest():
     """ Returns true if we are doing a test with gcov enabled """
     return os.environ.get('COVERAGE', '0') == '1'
+
 
 def extractFileToTmpFile(compressedFile):
     tempFileHandle, tempFileName = tempfile.mkstemp()
@@ -20,14 +22,16 @@ def extractFileToTmpFile(compressedFile):
             f.write(src.read())
     return tempFileName
 
+
 class TestBuild(unittest.TestCase):
     def test_make_wo_args_completes_returns_0(self):
         """ 'make -j4' should return 0 """
-        self.assertEqual(subprocess.call(['make', '-f', 'Makefile', '-j4'], stdout = devnull), 0)
+        self.assertEqual(subprocess.call(['make', '-f', 'Makefile', '-j4'], stdout=devnull), 0)
+
 
 class TestUsingBuildResult(unittest.TestCase):
     def buildDRAMPower(self):
-        self.assertEqual(subprocess.call(['make', '-f', 'Makefile', '-j4'], stdout = devnull), 0)
+        self.assertEqual(subprocess.call(['make', '-f', 'Makefile', '-j4'], stdout=devnull), 0)
 
     def setUp(self):
         self.buildDRAMPower()
@@ -48,11 +52,12 @@ class TestUsingBuildResult(unittest.TestCase):
                 # We don't really care, since it is a /tmp file anyway.
                 pass
 
+
 class TestOutput(TestUsingBuildResult):
     def run_and_compare_to_reference(self, cmd, referenceFile):
         self.maxDiff = None  # Show full diff on error.
         with open(self.tempFileName, 'w') as f:
-            subprocess.call(cmd, stdout = f)
+            subprocess.call(cmd, stdout=f)
 
         new = self.getFilteredOutput(self.tempFileName)
         ref = self.getFilteredOutput(referenceFile)
@@ -68,7 +73,7 @@ class TestOutput(TestUsingBuildResult):
 
     def test_no_arguments_error(self):
         """ running drampower w/o arguments returns 1 """
-        self.assertEqual(subprocess.call(['./drampower'], stdout = devnull), 1)
+        self.assertEqual(subprocess.call(['./drampower'], stdout=devnull), 1)
 
     def get_LPDDR2_1066_trace_file(self):
         cmdTrace = extractFileToTmpFile('test/data/LPDDR2-1066.commands.trace.gz')
@@ -105,13 +110,14 @@ class TestOutput(TestUsingBuildResult):
                                           '-m', 'memspecs/MICRON_1Gb_DDR2-800_16bit_H.xml',
                                           '-c', 'test/data/warnings.trace'], stdout=devnull, stderr=devnull), 0)
 
+
 class TestLibDRAMPower(TestUsingBuildResult):
     testPath = 'test/libdrampowertest'
 
-    def buildLibDRAMPowerExecutable(self, useXerces = True):
+    def buildLibDRAMPowerExecutable(self, useXerces=True):
         xerces = 'USE_XERCES=%d' % (1 if useXerces else 0)
         coverage = 'COVERAGE=%d' % (1 if inCoverageTest() else 0)
-        self.assertEqual(subprocess.call(['make', '-f', TestLibDRAMPower.testPath + '/Makefile', 'DRAMPOWER_PATH=.', xerces, coverage], stdout = devnull), 0)
+        self.assertEqual(subprocess.call(['make', '-f', TestLibDRAMPower.testPath + '/Makefile', 'DRAMPOWER_PATH=.', xerces, coverage], stdout=devnull), 0)
 
     def test_libdrampower_with_xerces_test_builds(self):
         """ libdrampower-based executable build should return 0 """
@@ -119,7 +125,7 @@ class TestLibDRAMPower(TestUsingBuildResult):
 
     def test_libdrampower_without_xerces_test_builds(self):
         """ libdrampower-based executable build should return 0 """
-        self.buildLibDRAMPowerExecutable(useXerces = False)
+        self.buildLibDRAMPowerExecutable(useXerces=False)
         if inCoverageTest():
             os.unlink('lib_test.gcno')
 
@@ -129,7 +135,7 @@ class TestLibDRAMPower(TestUsingBuildResult):
 
         with open(self.tempFileName, 'w') as f:
             self.assertEqual(subprocess.call([TestLibDRAMPower.testPath + '/library_test',
-                                              'memspecs/MICRON_1Gb_DDR2-1066_16bit_H.xml'], stdout = f), 0)
+                                              'memspecs/MICRON_1Gb_DDR2-1066_16bit_H.xml'], stdout=f), 0)
             try:
                 """ Copy coverage statistics to test subfolder. Otherwise the coverage tool gets confused. """
                 if inCoverageTest():
@@ -142,9 +148,10 @@ class TestLibDRAMPower(TestUsingBuildResult):
         ref = self.getFilteredOutput('test/reference/test_libdrampower_output_matches_reference.out')
         self.assertListEqual(new, ref)
 
+
 class TestClean(unittest.TestCase):
     def setUp(self):
-        self.assertEqual(subprocess.call(['make', '-f', 'Makefile', 'clean'], stdout = devnull), 0)
+        self.assertEqual(subprocess.call(['make', '-f', 'Makefile', 'clean'], stdout=devnull), 0)
 
     def test_make_clean_removes_compiler_output(self):
         """ 'make clean' should remove all .o and .a and .d files """
@@ -153,7 +160,7 @@ class TestClean(unittest.TestCase):
         for root, dirnames, files in os.walk('.'):
             for f in files:
                 count += 1 if fnmatch.fnmatch(f, pattern) else 0
-        self.assertEqual(count, 0, msg = self.shortDescription())
+        self.assertEqual(count, 0, msg=self.shortDescription())
 
 if __name__ == '__main__':
     with open(os.devnull, 'wb') as devnull:
