8c5751bdace2b8ecdaf9d614e2358ce6618471fe
lulys96 <58423317+lulys96@users.noreply.github.com>
2021-02-23 11:42:50 +0100

    Update MemSpecDDR4.cc

--- src/library/memspec/MemSpecDDR4.cc
+++ src/library/memspec/MemSpecDDR4.cc
@@ -49,7 +49,7 @@ MemSpecDDR4::MemSpecDDR4(nlohmann::json &memspec)
     numberOfRanks          = parseUint(memspec["memarchitecturespec"]["nbrOfRanks"],"nbrOfRanks");
     refreshMode            = parseUintWithDefaut(memspec["RefreshMode"],"RefreshMode",1);
     memTimingSpec.fCKMHz   = (parseUdouble(memspec["memtimingspec"]["clkMhz"], "clkMhz"));
-    memTimingSpec.tCK      = (1000.0 / memTimingSpec.fCKMHz); //clock period in mili seconds
+    memTimingSpec.tCK      = (1000.0 / memTimingSpec.fCKMHz); //clock period in nano seconds
     memTimingSpec.tCKESR   = (parseUint(memspec["memtimingspec"]["CKESR"], "CKESR"));
     memTimingSpec.tCKE     = (parseUint(memspec["memtimingspec"]["CKE"], "CKE"));
     memTimingSpec.tPD      = memTimingSpec.tCKE;
