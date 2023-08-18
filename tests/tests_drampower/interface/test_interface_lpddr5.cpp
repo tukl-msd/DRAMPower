9316f02b814b018619db3cc0cb0fc2977861d8c2
Alex Cani <alex.cani@iese.fraunhofer.de>
2023-08-18 15:32:31 +0200

    LPDDR5 IO Power tests

--- tests/tests_drampower/interface/test_interface_lpddr5.cpp
+++ tests/tests_drampower/interface/test_interface_lpddr5.cpp
@@ -208,3 +208,180 @@ TEST_F(LPDDR5_WindowStats_Tests, WriteClockOnDemand) {
     EXPECT_EQ(stats.wClockStats.ones_to_zeroes, wck_ones);
     EXPECT_EQ(stats.wClockStats.zeroes_to_ones, wck_ones);
 }
+
+// Tests for power consumption (given a known SimulationStats)
+class LPDDR5_Energy_Tests : public ::testing::Test {
+   public:
+    LPDDR5_Energy_Tests() {
+        std::ifstream f(std::string(TEST_RESOURCE_DIR) + "lpddr5.json");
+
+        if (!f.is_open()) {
+            std::cout << "Error: Could not open memory specification" << std::endl;
+            exit(1);
+        }
+
+        json data = json::parse(f);
+        spec = MemSpecLPDDR5{data["memspec"]};
+
+        t_CK = spec.memTimingSpec.tCK;
+        t_WCK = spec.memTimingSpec.tWCK;
+        voltage = spec.memPowerSpec[MemSpecLPDDR5::VoltageDomain::VDDQ].vDDX;
+
+        // Change impedances to different values from each other
+        spec.memImpedanceSpec.R_eq_cb = 2;
+        spec.memImpedanceSpec.R_eq_ck = 3;
+        spec.memImpedanceSpec.R_eq_dqs = 4;
+        spec.memImpedanceSpec.R_eq_rb = 5;
+        spec.memImpedanceSpec.R_eq_wb = 6;
+        spec.memImpedanceSpec.R_eq_wck = 7;
+
+        spec.memImpedanceSpec.C_total_cb = 2;
+        spec.memImpedanceSpec.C_total_ck = 3;
+        spec.memImpedanceSpec.C_total_dqs = 4;
+        spec.memImpedanceSpec.C_total_rb = 5;
+        spec.memImpedanceSpec.C_total_wb = 6;
+        spec.memImpedanceSpec.C_total_wck = 7;
+
+        io_calc = std::make_unique<InterfaceCalculation_LPDDR5>(spec);
+    }
+
+    MemSpecLPDDR5 spec;
+    double t_CK;
+    double t_WCK;
+    double voltage;
+    std::unique_ptr<InterfaceCalculation_LPDDR5> io_calc;
+};
+
+TEST_F(LPDDR5_Energy_Tests, Parameters) {
+    ASSERT_TRUE(t_CK > 0.0);
+    ASSERT_TRUE(voltage > 0.0);
+    ASSERT_TRUE(t_WCK > 0.0);
+}
+
+TEST_F(LPDDR5_Energy_Tests, Clock_Energy) {
+    SimulationStats stats;
+    stats.clockStats.ones = 43;
+    stats.clockStats.zeroes_to_ones = 47;
+    stats.clockStats.zeroes = 53;
+    stats.clockStats.ones_to_zeroes = 59;
+
+    stats.wClockStats.ones = 61;
+    stats.wClockStats.zeroes = 67;
+    stats.wClockStats.zeroes_to_ones = 71;
+    stats.wClockStats.ones_to_zeroes = 73;
+
+    interface_energy_info_t result = io_calc->calculateEnergy(stats);
+    // Clock is provided by the controller not the device
+    EXPECT_DOUBLE_EQ(result.dram.dynamicPower, 0.0);
+    EXPECT_DOUBLE_EQ(result.dram.staticPower, 0.0);
+
+    // Clock is differential so there is always going to be one signal that consumes power
+    // Calculation is done considering number of ones but could also be zeroes, since clock is
+    // a symmetrical signal
+    double expected_static = stats.clockStats.ones * voltage * voltage * t_CK / spec.memImpedanceSpec.R_eq_ck;
+    expected_static += stats.wClockStats.ones * voltage * voltage * t_WCK / spec.memImpedanceSpec.R_eq_wck;
+
+    double expected_dynamic = stats.clockStats.zeroes_to_ones * spec.memImpedanceSpec.C_total_ck * voltage * voltage;
+    expected_dynamic += stats.wClockStats.zeroes_to_ones * spec.memImpedanceSpec.C_total_wck * voltage * voltage;
+
+    EXPECT_DOUBLE_EQ(result.controller.staticPower, expected_static);  // value itself doesn't matter, only that it matches the formula
+    EXPECT_DOUBLE_EQ(result.controller.dynamicPower, expected_dynamic);
+    EXPECT_TRUE(result.controller.staticPower > 0.0);
+    EXPECT_TRUE(result.controller.dynamicPower > 0.0);
+}
+
+TEST_F(LPDDR5_Energy_Tests, DQS_Energy) {
+    SimulationStats stats;
+    stats.readDQSStats.ones = 200;
+    stats.readDQSStats.zeroes = 400;
+    stats.readDQSStats.zeroes_to_ones = 700;
+    stats.readDQSStats.ones_to_zeroes = 1000;
+
+    // Write DQS is not used in LPDDR5, instead there is WCK
+    stats.writeDQSStats.ones = 300;
+    stats.writeDQSStats.zeroes = 100;
+    stats.writeDQSStats.ones_to_zeroes = 2000;
+    stats.writeDQSStats.zeroes_to_ones = 999;
+
+    // Term power if consumed by 1's
+    // Controller -> write power
+    // Dram -> read power
+    double expected_static_controller = 0.0;
+    double expected_static_dram =
+        0.5 * stats.readDQSStats.ones * voltage * voltage * t_CK / spec.memImpedanceSpec.R_eq_dqs;
+
+    // Dynamic power is consumed on 0 -> 1 transition
+    double expected_dynamic_controller = 0;
+    double expected_dynamic_dram = stats.readDQSStats.zeroes_to_ones *
+                                   spec.memImpedanceSpec.C_total_dqs / 2.0 * voltage * voltage;
+
+    interface_energy_info_t result = io_calc->calculateEnergy(stats);
+    EXPECT_DOUBLE_EQ(result.controller.staticPower, expected_static_controller);
+    EXPECT_DOUBLE_EQ(result.controller.dynamicPower, expected_dynamic_controller);
+    EXPECT_DOUBLE_EQ(result.dram.staticPower, expected_static_dram);
+    EXPECT_DOUBLE_EQ(result.dram.dynamicPower, expected_dynamic_dram);
+
+    EXPECT_TRUE(result.dram.staticPower > 0.0);
+    EXPECT_TRUE(result.dram.dynamicPower > 0.0);
+}
+
+TEST_F(LPDDR5_Energy_Tests, DQ_Energy) {
+    SimulationStats stats;
+    stats.readBus.ones = 7;
+    stats.readBus.zeroes = 11;
+    stats.readBus.zeroes_to_ones = 19;
+    stats.readBus.ones_to_zeroes = 39;
+
+    stats.writeBus.ones = 43;
+    stats.writeBus.zeroes = 59;
+    stats.writeBus.zeroes_to_ones = 13;
+    stats.writeBus.ones_to_zeroes = 17;
+
+    // Term power if consumed by 1's on LPDDR5 (pulldown terminated)
+    // Controller -> write power
+    // Dram -> read power
+    double expected_static_controller =
+        0.5 * stats.writeBus.ones * voltage * voltage * t_CK / spec.memImpedanceSpec.R_eq_wb;
+    double expected_static_dram =
+        0.5 * stats.readBus.ones * voltage * voltage * t_CK / spec.memImpedanceSpec.R_eq_rb;
+
+    // Dynamic power is consumed on 0 -> 1 transition
+    double expected_dynamic_controller =
+        stats.writeBus.zeroes_to_ones * spec.memImpedanceSpec.C_total_wb / 2.0 * voltage * voltage;
+    double expected_dynamic_dram =
+        stats.readBus.zeroes_to_ones * spec.memImpedanceSpec.C_total_rb / 2.0 * voltage * voltage;
+
+    interface_energy_info_t result = io_calc->calculateEnergy(stats);
+    EXPECT_DOUBLE_EQ(result.controller.staticPower, expected_static_controller);
+    EXPECT_DOUBLE_EQ(result.controller.dynamicPower, expected_dynamic_controller);
+    EXPECT_DOUBLE_EQ(result.dram.staticPower, expected_static_dram);
+    EXPECT_DOUBLE_EQ(result.dram.dynamicPower, expected_dynamic_dram);
+
+    EXPECT_TRUE(result.controller.staticPower > 0.0);
+    EXPECT_TRUE(result.controller.dynamicPower > 0.0);
+    EXPECT_TRUE(result.dram.staticPower > 0.0);
+    EXPECT_TRUE(result.dram.dynamicPower > 0.0);
+}
+
+TEST_F(LPDDR5_Energy_Tests, CA_Energy) {
+    SimulationStats stats;
+    stats.commandBus.ones = 11;
+    stats.commandBus.zeroes = 29;
+    stats.commandBus.zeroes_to_ones = 39;
+    stats.commandBus.ones_to_zeroes = 49;
+
+    double expected_static_controller = 0.5 * stats.commandBus.zeroes * voltage * voltage * t_CK / spec.memImpedanceSpec.R_eq_cb;
+    double expected_dynamic_controller = stats.commandBus.zeroes_to_ones * spec.memImpedanceSpec.C_total_cb / 2.0 * voltage * voltage;
+
+    interface_energy_info_t result = io_calc->calculateEnergy(stats);
+
+    // CA bus power is provided by the controller
+    EXPECT_DOUBLE_EQ(result.dram.dynamicPower, 0.0);
+    EXPECT_DOUBLE_EQ(result.dram.staticPower, 0.0);
+
+    EXPECT_DOUBLE_EQ(result.controller.staticPower, expected_static_controller);
+    EXPECT_DOUBLE_EQ(result.controller.dynamicPower, expected_dynamic_controller);
+
+    EXPECT_TRUE(result.controller.staticPower > 0.0);
+    EXPECT_TRUE(result.controller.dynamicPower > 0.0);
+}
