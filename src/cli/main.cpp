aeaf364d04b77057081065e00e7b6e9d20a1538c
marcomoerz <marco.moerz@iese.fraunhofer.de>
2024-08-15 12:02:31 +0200

    added cli11 and spdlog to cli

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -28,6 +28,10 @@
 #include <DRAMUtils/memspec/standards/MemSpecLPDDR4.h>
 #include <DRAMUtils/memspec/standards/MemSpecLPDDR5.h>
 
+#include <cli11/CLI11.hpp>
+#include <spdlog/spdlog.h>
+#include <spdlog/fmt/ostr.h>
+
 #include "csv.hpp"
 #include "util.hpp"
 
@@ -39,9 +43,12 @@ std::unique_ptr<dram_base<CmdType>> getMemory(const std::string_view &data)
 	try
 	{
 		std::unique_ptr<dram_base<CmdType>> result = nullptr;
+		// Get memspec
         auto memspec = DRAMUtils::parse_memspec_from_file(std::filesystem::path(data));
-        if (!memspec)
+        if (!memspec) {
             return result;
+		}
+		// Get ddr
 		std::visit( [&result] (auto&& arg) {
 			using T = std::decay_t<decltype(arg)>;
 			if constexpr (std::is_same_v<T, DRAMUtils::MemSpec::MemSpecDDR4>)
@@ -69,20 +76,13 @@ std::unique_ptr<dram_base<CmdType>> getMemory(const std::string_view &data)
 	}
 	catch(const std::exception& e)
 	{
-		std::cerr << e.what() << '\n';
+		spdlog::error(e.what());
 		return nullptr;
 	}
 }
 
 std::vector<std::pair<Command, std::unique_ptr<uint8_t[]>>> parse_command_list(std::string_view csv_file)
 {
-	// Check if csv file exists
-	if ( !std::filesystem::exists(csv_file) || std::filesystem::is_directory(csv_file) )
-	{
-		std::cerr << "CSV file was not found!" << std::endl;
-		exit(1);
-	}
-
 	std::vector<std::pair<Command, std::unique_ptr<uint8_t[]>>> commandList;
 
 	// Read csv file
@@ -94,19 +94,11 @@ std::vector<std::pair<Command, std::unique_ptr<uint8_t[]>>> parse_command_list(s
 	
 	// loop variables
 	uint64_t rowcounter = 0;
-	std::size_t rowidx = 0;
-	std::size_t size = 0;
-
-	std::size_t rank_id = 0;
-	std::size_t bank_group_id = 0;
-	std::size_t bank_id = 0;
-	std::size_t row_id = 0;
-	std::size_t column_id = 0;
+	std::size_t rowidx, size, rank_id, bank_group_id, bank_id, row_id, column_id = 0;
 
 	timestamp_t timestamp = 0;
 	csv::string_view cmdType;
 	std::unordered_map<csv::string_view, DRAMPower::CmdType>::iterator cmdit;
-	DRAMPower::CmdType *datait;
 	CmdType cmd;
 
 	// Parse csv file
@@ -116,7 +108,7 @@ std::vector<std::pair<Command, std::unique_ptr<uint8_t[]>>> parse_command_list(s
 		rowidx = 0;
 
 		// Read csv row
-		if ( row.size() < 7 )
+		if ( row.size() < MINCSVSIZE )
 		{
 			std::cerr << "Invalid command structure. Row " << rowcounter << std::endl;
 			exit(1);
@@ -134,13 +126,12 @@ std::vector<std::pair<Command, std::unique_ptr<uint8_t[]>>> parse_command_list(s
 		cmd = DRAMPower::CmdTypeUtil::from_string(cmdType);
 
 		// Get data if needed
-		if ( DRAMPower::CmdTypeUtil::needs_data(cmd) )
-		{
-			if ( row.size() < 8 ){
+		if ( DRAMPower::CmdTypeUtil::needs_data(cmd) ) {
+			if ( row.size() < MINCSVSIZE + 1 ) {
 				std::cerr << "Invalid command structure. Row " << rowcounter << std::endl;
 				exit(1);
 			}
-			uint64_t length = 0;
+			// uint64_t length = 0;
 			csv::string_view data = row[rowidx++].get_sv();
 			std::unique_ptr<uint8_t[]> arr;
 			try
@@ -155,8 +146,7 @@ std::vector<std::pair<Command, std::unique_ptr<uint8_t[]>>> parse_command_list(s
 			}
 			commandList.emplace_back(Command{ timestamp, cmd, { bank_id, bank_group_id, rank_id, row_id, column_id}, arr.get(), size * 8}, std::move(arr));
 		}
-		else
-		{
+		else {
 			commandList.emplace_back(Command{ timestamp, cmd, { bank_id, bank_group_id, rank_id, row_id, column_id} }, nullptr);
 		}
 		// Increment row counter
@@ -166,47 +156,122 @@ std::vector<std::pair<Command, std::unique_ptr<uint8_t[]>>> parse_command_list(s
 	return commandList;
 };
 
-int main(int argc, char *argv[])
+void jsonFileResult(const std::string &jsonfile, const std::unique_ptr<dram_base<CmdType>> &ddr, const energy_t &core_energy, const interface_energy_info_t &interface_energy)
 {
-	bool to_json = false;
 	std::ofstream out;
-
-	// Check number of arguments
-	if ( argc != 3 && argc != 5) {
-		std::cerr << "Usage: ./CLI command_list.csv memspec [--json output_file]" << std::endl;
+	out = std::ofstream(jsonfile);
+	if ( !out.is_open() ) {
+		std::cerr << "Could not open file " << jsonfile << std::endl;
 		exit(1);
 	}
+	json_t j;
+	size_t energy_offset = 0;
+	auto bankcount = ddr->getBankCount();
+	auto rankcount = ddr->getRankCount();
+	auto devicecount = ddr->getDeviceCount();
 
-	// Parse command list
-	auto commandList = parse_command_list(argv[1]);
+	j["RankCount"] = ddr->getRankCount();
+	j["DeviceCount"] = ddr->getDeviceCount();
+	j["BankCount"] = ddr->getBankCount();
+	j["TotalEnergy"] = core_energy.total() + interface_energy.total();
 
-	// Initialize memory
-	// Create memory object
-	std::unique_ptr<dram_base<CmdType>> ddr = getMemory(std::string_view(argv[2]));
-	if (!ddr)
+	// Energy object to json
+	core_energy.to_json(j["CoreEnergy"]);
+	interface_energy.to_json(j["InterfaceEnergy"]);
+
+	// Validate array length
+	if ( !j["CoreEnergy"][core_energy.get_Bank_energy_keyword()].is_array() || j["CoreEnergy"][core_energy.get_Bank_energy_keyword()].size() != rankcount * bankcount * devicecount )
 	{
+		assert(false); // (should not happen)
+		std::cerr << "Invalid energy array length" << std::endl;
+		exit(1);
+	}
+	
+	// Add rank,device,bank description
+	for ( std::size_t r = 0; r < rankcount; r++ ) {
+		for ( std::size_t d = 0; d < devicecount; d++ ) {
+			energy_offset = r * bankcount * devicecount + d * bankcount;
+			for ( std::size_t b = 0; b < bankcount; b++ ) {
+				// Rank,Device,Bank -> bank_energy
+				j["CoreEnergy"][core_energy.get_Bank_energy_keyword()].at(energy_offset + b)["Rank"] = r;
+				j["CoreEnergy"][core_energy.get_Bank_energy_keyword()].at(energy_offset + b)["Device"] = d;
+				j["CoreEnergy"][core_energy.get_Bank_energy_keyword()].at(energy_offset + b)["Bank"] = b;
+			}
+		}
+	}
+	
+	out << j.dump(4) << std::endl;
+	out.close();
+}
+
+void stdoutResult(const std::unique_ptr<dram_base<CmdType>> &ddr, const energy_t &core_energy, const interface_energy_info_t &interface_energy)
+{
+	// Setup output format
+	std::cout << std::defaultfloat << std::setprecision(3);
+	// Print stats
+	auto bankcount = ddr->getBankCount();
+	auto rankcount = ddr->getRankCount();
+	auto devicecount = ddr->getDeviceCount();
+	size_t energy_offset = 0;
+	// NOTE: ensure the same order of calculation in the interface
+	spdlog::info("Rank,Device,Bank -> bank_energy:");
+	for ( std::size_t r = 0; r < rankcount; r++ ) {
+		for ( std::size_t d = 0; d < devicecount; d++ ) {
+			energy_offset = r * bankcount * devicecount + d * bankcount;
+			for ( std::size_t b = 0; b < bankcount; b++ ) {
+				// Rank,Device,Bank -> bank_energy
+				spdlog::info("{},{},{} -> {}",
+					r, d, b,
+					core_energy.bank_energy[energy_offset + b]
+				);
+			}
+		}
+	}
+	spdlog::info("Cumulated bank energy with bg_act_shared -> {}", core_energy.total_energy());
+	spdlog::info("Shared energy -> {}", core_energy);
+	spdlog::info("Interface Energy:\n{}", interface_energy);
+	spdlog::info("Total Energy -> {}", core_energy.total() + interface_energy.total());
+}
+
+int main(int argc, char *argv[])
+{
+	// Application description
+	CLI::App app{"DRAMPower v" DRAMPOWER_VERSION_STRING};
+	argv = app.ensure_utf8(argv);
+	// Options
+	std::string tracefile;
+	std::string memspec;
+	std::optional<std::string> jsonfile = std::nullopt;
+	// Tracefile
+	app.add_option("-t,--trace", tracefile, "csv trace file")
+		->required(true)
+		->check(CLI::ExistingFile);
+	// Memspec
+	app.add_option("-m,--memspec", memspec, "json memspec file")
+		->required(true)
+		->check(CLI::ExistingFile);
+	// JSON output file
+	app.add_option("-j,--json", jsonfile, "json output file path")
+		->required(false)
+		->check(CLI::ExistingFile);
+	// Parse arguments
+	CLI11_PARSE(app, argc, argv);
+
+	// Set spdlog pattern
+	spdlog::set_pattern("%v");
+
+	// Parse command list (load command list in memory)
+	auto commandList = parse_command_list(tracefile);
+
+	// Initialize memory / Create memory object
+	std::unique_ptr<dram_base<CmdType>> ddr = getMemory(std::string_view(memspec));
+	if (!ddr) {
 		std::cerr << "Invalid memory specification" << std::endl;
 		exit(1);
 	}
 
     // TODO set togglingrate
     ddr->setToggleRate(std::nullopt);
-
-	// Get json argument
-	if ( argc == 5 )
-	{
-		std::string_view arg(argv[3]);
-		if ( arg == "--json" )
-		{
-			to_json = true;
-			out = std::ofstream(argv[4]);
-			if ( !out.is_open() )
-			{
-				std::cerr << "Could not open file " << argv[4] << std::endl;
-				exit(1);
-			}
-		}
-	}
 
 	// Execute commands
 	for ( auto &command : commandList ) {
@@ -218,87 +283,13 @@ int main(int argc, char *argv[])
     interface_energy_info_t interface_energy = ddr->calcInterfaceEnergy(commandList.back().first.timestamp);
 	auto stats = ddr->getStats();
 
-	if(to_json == false)
+	if(jsonfile)
 	{
-		// Setup output format
-		std::cout << std::defaultfloat << std::setprecision(3);
-
-		// Print stats
-		auto bankcount = ddr->getBankCount();
-		auto rankcount = ddr->getRankCount();
-		auto devicecount = ddr->getDeviceCount();
-		size_t energy_offset = 0;
-		// TODO assumed this order in interface calculation
-		std::cout << "Rank,Device,Bank -> bank_energy:" << std::endl;
-		for ( std::size_t r = 0; r < rankcount; r++ ) {
-			for ( std::size_t d = 0; d < devicecount; d++ ) {
-				energy_offset = r * bankcount * devicecount + d * bankcount;
-				for ( std::size_t b = 0; b < bankcount; b++ ) {
-					// Rank,Device,Bank -> bank_energy
-					std::cout << r << "," << d << "," << b << " -> ";
-					std::cout << core_energy.bank_energy[energy_offset + b] << "\n";
-				}
-			}
-		}
-		std::cout << "\n";
-		// All banks summed up and bg act shared added
-		std::cout << "Cumulated bank energy with bg_act_shared -> " << core_energy.total_energy() << "\n";
-
-
-		// Background energy of all ranks
-		std::cout << "Shared energy -> " << core_energy << "\n\n";
-
-        // Interface energy
-        std::cout << "Interface Energy: " << "\n";
-        std::cout << interface_energy << std::endl;
-
-		// Total energy
-		std::cout << "Total Energy -> " << core_energy.total() + interface_energy.total();
-		// \n and flush
-		std::cout << std::endl;
+		jsonFileResult(*jsonfile, std::move(ddr), core_energy, interface_energy);
 	}
 	else
 	{
-		// Assume out is valid
-		json_t j;
-		size_t energy_offset = 0;
-		auto bankcount = ddr->getBankCount();
-		auto rankcount = ddr->getRankCount();
-		auto devicecount = ddr->getDeviceCount();
-
-		j["RankCount"] = ddr->getRankCount();
-		j["DeviceCount"] = ddr->getDeviceCount();
-		j["BankCount"] = ddr->getBankCount();
-		j["TotalEnergy"] = core_energy.total() + interface_energy.total();
-
-		// Energy object to json
-		core_energy.to_json(j["CoreEnergy"]);
-        interface_energy.to_json(j["InterfaceEnergy"]);
-
-		// Validate array length
-		if ( !j["CoreEnergy"][core_energy.get_Bank_energy_keyword()].is_array() || j["CoreEnergy"][core_energy.get_Bank_energy_keyword()].size() != rankcount * bankcount * devicecount )
-		{
-			assert(false); // (should not happen)
-			std::cerr << "Invalid energy array length" << std::endl;
-			exit(1);
-		}
-		
-		// Add rank,device,bank description
-		for ( std::size_t r = 0; r < rankcount; r++ ) {
-			for ( std::size_t d = 0; d < devicecount; d++ ) {
-				energy_offset = r * bankcount * devicecount + d * bankcount;
-				for ( std::size_t b = 0; b < bankcount; b++ ) {
-					// Rank,Device,Bank -> bank_energy
-					j["CoreEnergy"][core_energy.get_Bank_energy_keyword()].at(energy_offset + b)["Rank"] = r;
-					j["CoreEnergy"][core_energy.get_Bank_energy_keyword()].at(energy_offset + b)["Device"] = d;
-					j["CoreEnergy"][core_energy.get_Bank_energy_keyword()].at(energy_offset + b)["Bank"] = b;
-				}
-			}
-		}
-		
-		out << j.dump(4) << std::endl;
-		out.close();
+		stdoutResult(std::move(ddr), core_energy, interface_energy);
 	}
-
 	return 0;
 };
