12b9071994ab27ffa4ba4b0f0d657442ca291008
marcomoerz <moerz@rptu.de>
2025-07-10 12:16:19 +0200

    removed TODOs in Rank::isActive
    keeping the current implementation for consistency and compatibility

--- src/DRAMPower/DRAMPower/dram/Rank.cpp
+++ src/DRAMPower/DRAMPower/dram/Rank.cpp
@@ -10,15 +10,9 @@ Rank::Rank(std::size_t numBanks)
 
 bool Rank::isActive(timestamp_t timestamp) {
     if ( timestamp < this->endRefreshTime ) {
-        // TODO Invalid in test cases
-        // Implicit given in the dram implementation
         std::cout << "[WARN] Rank::isActive() -> timestamp (" << timestamp <<") < "  << "endRefreshTime (" << this->endRefreshTime << ")"  << std::endl;
     }
-    return countActiveBanks() > 0 || timestamp < this->endRefreshTime;    // TODO check for endRefreshTime not necessary -> implicit
-    /*
-        TODO theoretisch check für timestamp < endRefreshTime nicht notwending,
-        wenn der Memory Zustand vor dem Check gesetzt wird
-    */
+    return countActiveBanks() > 0 || timestamp < this->endRefreshTime;
 }
 
 std::size_t Rank::countActiveBanks() const {
