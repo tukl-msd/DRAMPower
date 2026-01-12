d92d583014986f31b8c2395439f03a7bcb5a7086
marcomoerz <64926170+marcomoerz@users.noreply.github.com>
2026-01-13 00:25:18 +0100

    feat: added CoreRegisterHelperNoRank

--- src/DRAMPower/DRAMPower/util/RegisterHelper.h
+++ src/DRAMPower/DRAMPower/util/RegisterHelper.h
@@ -86,6 +86,88 @@ private:
     std::vector<Rank> &m_ranks;
 };
 
+template<typename Core, typename BankExtractor>
+struct CoreRegisterHelperNoRank {
+// Public constructors and assignment operators
+public:
+    using commandCounter_t = util::CommandCounter<CmdType>;
+
+    template<typename... Args>
+    CoreRegisterHelperNoRank(Core *core, std::vector<Bank> &banks, commandCounter_t &commandCounter, Args&&... args)
+        : m_core(core)
+        , m_banks(banks)
+        , m_commandCounter(commandCounter)
+        , m_bankExtractor(std::forward<Args>(args)...)
+    {}
+    CoreRegisterHelperNoRank(const CoreRegisterHelperNoRank&) = delete; // No copy constructor
+    CoreRegisterHelperNoRank& operator=(const CoreRegisterHelperNoRank&) = delete; // No copy assignment operator
+    CoreRegisterHelperNoRank(CoreRegisterHelperNoRank&&) = default; // Move constructor
+    CoreRegisterHelperNoRank& operator=(CoreRegisterHelperNoRank&&) = default; // Move assignment operator
+    ~CoreRegisterHelperNoRank() = default; // Destructor
+
+// Public member functions
+public:
+    template<typename Func>
+    decltype(auto) registerBankHandler(Func &&member_func) {
+        Core* this_ptr = m_core;
+        return [this, this_ptr, banks_ref = std::ref(m_banks), commandCounter_ref = std::ref(m_commandCounter), f = std::forward<Func>(member_func)](const Command & command) {
+            auto &this_banks = banks_ref.get();
+            auto &this_commandCounter = commandCounter_ref.get();
+
+            std::size_t bank_id = m_bankExtractor(command.targetCoordinate);
+            assert(this_banks.size()>bank_id);
+            auto & bank = this_banks.at(bank_id);
+
+            (this_ptr->*f)(bank, command.timestamp);
+            this_commandCounter.inc(command.type);
+        };
+    }
+
+    template<typename Func>
+    decltype(auto) registerRankHandler(Func &&member_func) {
+        Core* this_ptr = m_core;
+        return [this_ptr, commandCounter_ref = std::ref(m_commandCounter), f = std::forward<Func>(member_func)](const Command & command) mutable {
+            auto &this_commandCounter = commandCounter_ref.get();
+            (this_ptr->*f)(command.timestamp);
+            this_commandCounter.inc(command.type);
+        };
+    }
+
+    template <typename Func>
+    decltype(auto) registerBankGroupHandler(Func &&member_func) {
+        Core *this_ptr = m_core;
+        return [this, this_ptr, banks_ref = std::ref(m_banks), commandCounter_ref = std::ref(m_commandCounter), f = std::forward<Func>(member_func)](const Command & command) {
+            auto &this_banks = banks_ref.get();
+            auto &this_commandCounter = commandCounter_ref.get();
+
+            std::size_t bank_id = m_bankExtractor(command.targetCoordinate);
+            assert(this_banks.size()>bank_id);
+            if (command.targetCoordinate.bank >= this_banks.size()) {
+                throw std::invalid_argument("Invalid bank targetcoordinate");
+            }
+            (this_ptr->*f)(bank_id, command.timestamp);
+            this_commandCounter.inc(command.type);
+        };
+    }
+
+    template<typename Func>
+    decltype(auto) registerHandler(Func &&member_func) {
+        Core* this_ptr = m_core;
+        return [commandCounter_ref = std::ref(m_commandCounter), this_ptr, f = std::forward<Func>(member_func)](const Command & command) {
+            auto &this_commandCounter = commandCounter_ref.get();
+            (this_ptr->*f)(command.timestamp);
+            this_commandCounter.inc(command.type);
+        };
+    }
+
+// Private member variables
+private:
+    Core *m_core;
+    std::vector<Bank> &m_banks;
+    commandCounter_t &m_commandCounter;
+    BankExtractor m_bankExtractor;
+};
+
 template<typename Interface>
 struct InterfaceRegisterHelper {
 // Public constructors and assignment operators
