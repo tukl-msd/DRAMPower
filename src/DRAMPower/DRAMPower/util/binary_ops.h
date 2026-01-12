338e0473a4e6c0f7fb649ef1c1f39b75c06d5093
marcomoerz <64926170+marcomoerz@users.noreply.github.com>
2026-01-13 00:42:14 +0100

    refactor: improved binary_ops implementation

--- src/DRAMPower/DRAMPower/util/binary_ops.h
+++ src/DRAMPower/DRAMPower/util/binary_ops.h
@@ -1,8 +1,26 @@
 #ifndef DRAMPOWER_UTIL_BINARY_OPS_H
 #define DRAMPOWER_UTIL_BINARY_OPS_H
 
-#include <cstdint>
 #include <sstream>
+#include <bitset>
+
+#if defined(__GNUC__)
+	#define POPCOUNT __builtin_popcountll
+#elif defined(_MSC_VER)
+	#include <nmmintrin.h>
+	#if defined(_M_X64)
+		#define POPCOUNT _mm_popcnt_u64
+	#else
+		#define POPCOUNT _mm_popcnt_u32
+	#endif
+#elif defined(__cpp_lib_bitops)
+	#include <bit>
+	#define POPCOUNT std::popcount
+#else
+	#include <bitset>
+	inline std::size_t popcount_(uint64_t n) { return std::bitset<64>(n).count(); };
+	#define POPCOUNT popcount_
+#endif
 
 namespace DRAMPower::util {
 
@@ -20,15 +38,13 @@ inline std::string to_string(const T& bitset)
 };
 
 struct BinaryOps {
-    static std::size_t popcount(uint8_t n);
-    static std::size_t popcount(uint64_t n);
-    static std::size_t zero_to_ones(uint64_t p, uint64_t q);
-    static std::size_t one_to_zeroes(uint64_t p, uint64_t q);
-    static std::size_t bit_changes(uint64_t p, uint64_t q);
-
-    // overloads for std::bitset
     template <typename T>
-    static std::size_t popcount(const T& bitset)
+    static std::size_t popcount(const T& bitset) {
+        return POPCOUNT(bitset);
+    }
+
+    template <std::size_t N>
+    static std::size_t popcount(const std::bitset<N>& bitset)
     {
         return bitset.count();
     }
