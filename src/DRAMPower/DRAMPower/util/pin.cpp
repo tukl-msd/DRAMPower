ca605746fb169c8d4013722522371b7948ebfbbc
marcomoerz <64926170+marcomoerz@users.noreply.github.com>
2026-02-05 12:15:28 +0100

    feat(pin): add temp change functionality

--- src/DRAMPower/DRAMPower/util/pin.cpp
+++ src/DRAMPower/DRAMPower/util/pin.cpp
@@ -6,12 +6,22 @@ Pin::Pin(PinState state)
 : m_last_state(state)
 {}
 
-void Pin::addPendingStats(timestamp_t t, pin_stats_t &stats) const {
+void Pin::addPendingStats(timestamp_t t, PendingStats<PinPendingStats> pending_stats, pin_stats_t &stats) const {
     // add stats from last load stored in pending_stats
     if (pending_stats.isPending()) {
         assert(pending_stats.getTimestamp() < t);
         stats += getPinChangeStats(pending_stats.getStats().fromstate, pending_stats.getStats().newstate);
     }
+}
+
+void Pin::addPendingStats(timestamp_t t, timestamp_t pending_t, PinState to, PinState from, pin_stats_t &stats) const {
+
+    auto pending_change = PendingStats<PinPendingStats>{};
+    pending_change.setPendingStats(pending_t, {
+        from, // from
+        to    // to
+    });
+    addPendingStats(t, pending_change, stats);
 }
 
 Pin::pin_stats_t Pin::getPinChangeStats(const PinState &fromState, const PinState &newState) const {
@@ -31,14 +41,17 @@ Pin::pin_stats_t Pin::getPinChangeStats(const PinState &fromState, const PinStat
     return stats;
 }
 
-void Pin::count(timestamp_t timestamp, pin_stats_t &stats) const {
+void Pin::count(timestamp_t end, timestamp_t start, const PinState& state, pin_stats_t &stats) const {
+    if (end <= start) {
+        return;
+    }
     // Add duration of lastState
-    switch (m_last_state) {
+    switch (state) {
         case PinState::L:
-            stats.zeroes += timestamp - m_last_set;
+            stats.zeroes += end - start;
             break;
         case PinState::H:
-            stats.ones += timestamp - m_last_set;
+            stats.ones += end - start;
             break;
         case PinState::Z:
             // Nothing to do
@@ -46,16 +59,24 @@ void Pin::count(timestamp_t timestamp, pin_stats_t &stats) const {
     }
 }
 
-void Pin::set(timestamp_t t, PinState state, std::size_t dataRate)
-{
+void Pin::set_internal(timestamp_t t, PinState state, std::size_t dataRate, PendingStats<PinPendingStats> pending_stats, pin_stats_t &stats) const {
+    // Check if change is needed
+    if (m_last_state == state) {
+        return;
+    }
     timestamp_t virtual_time = t * dataRate;
     assert(virtual_time > m_last_set);
     // count stats
-    addPendingStats(virtual_time, m_stats);
-    pending_stats.clear();
-    count(virtual_time, m_stats);
-    // store new state
-    pending_stats.setPendingStats(virtual_time, {
+    addPendingStats(virtual_time, pending_stats, stats);
+    count(virtual_time, m_last_set, m_last_state, stats);
+}
+
+void Pin::set(timestamp_t t, PinState state, std::size_t dataRate)
+{
+    timestamp_t virtual_time = t * dataRate;
+    set_internal(t, state, dataRate, m_pending_stats, m_stats);
+    m_pending_stats.clear();
+    m_pending_stats.setPendingStats(virtual_time, {
         m_last_state, // From state
         state // New state
     });
@@ -63,42 +84,60 @@ void Pin::set(timestamp_t t, PinState state, std::size_t dataRate)
     m_last_state = state;
 }
 
-PinState Pin::get(timestamp_t timestamp, std::size_t dataRate)
+PinState Pin::get(timestamp_t timestamp, std::size_t dataRate) const
 {
     timestamp_t virtual_time = timestamp * dataRate;
     assert(virtual_time >= m_last_set);
     if (virtual_time == m_last_set) {
         // Retrive lastState from pendingStats
-        if (pending_stats.isPending()) {
-            return pending_stats.getStats().fromstate;
+        if (m_pending_stats.isPending()) {
+            return m_pending_stats.getStats().fromstate;
         } else {
             return m_last_state; // No pending stats, return last state
         }
     }
-    // Add stats before returning the state
-    if (pending_stats.isPending() && pending_stats.getTimestamp() < virtual_time) {
-        addPendingStats(virtual_time, m_stats);
-        pending_stats.clear();
-    }
-    // m_last_state doesn't change
-    count(virtual_time, m_stats);
-    m_last_set = virtual_time;
+    // virtual_time > m_last_set
     return m_last_state; // Return the last state
 }
 
-Pin::pin_stats_t Pin::get_stats_at(timestamp_t t, std::size_t dataRate) const
+Pin::pin_stats_t Pin::get_stats_at(timestamp_t t, std::size_t dataRate, std::optional<std::reference_wrapper<const PinTempChange>> optionalstats) const
 {
     timestamp_t virtual_time = t * dataRate;
     assert(virtual_time >= m_last_set);
     if (virtual_time == m_last_set) {
         return m_stats;
     }
-    // virtual_time > m_last_set
-
     // Add stats from m_last_set to t
     auto stats = m_stats;
-    addPendingStats(virtual_time, stats);
-    count(virtual_time, stats);
+    // virtual_time > m_last_set
+    if (optionalstats && m_pending_stats.isPending()) {
+        // optionalstats && pending_stats
+        const auto& optstats_v = optionalstats->get();
+        if (optstats_v.change_time <= m_pending_stats.getTimestamp()) {
+            // Only use pending_stats
+            addPendingStats(virtual_time, m_pending_stats, stats);
+            count(virtual_time, m_last_set, m_last_state, stats);
+        } else {
+            // optstats_v.change_time > m_pending_stats.getTimestamp()
+            // Add pendingstats, Count to optional change, Add optional change, Count to end
+            addPendingStats(virtual_time, m_pending_stats, stats);
+            count(optstats_v.change_time, m_last_set, m_pending_stats.getStats().newstate, stats);
+            addPendingStats(virtual_time, optstats_v.change_time, optstats_v.state, m_pending_stats.getStats().newstate, stats);
+            count(virtual_time, optstats_v.change_time, optstats_v.state, stats);
+        }
+    } else if (optionalstats/* && !m_pending_stats.isPending()*/) {
+        // optionalstats
+        // Count to optional change, Add optional change, Count to end
+        const auto& optstats_v = optionalstats->get();
+        count(optstats_v.change_time, m_last_set, m_last_state, stats);
+        addPendingStats(virtual_time, optstats_v.change_time, optstats_v.state, m_last_state, stats);
+        count(virtual_time, optstats_v.change_time, optstats_v.state, stats);
+    } else {
+        // pendingchange || !pending_stats
+        // Add pendingstats, count to end
+        addPendingStats(virtual_time, m_pending_stats, stats);
+        count(virtual_time, m_last_set, m_last_state, stats);
+    }
 
     return stats;
 }
@@ -106,14 +145,14 @@ Pin::pin_stats_t Pin::get_stats_at(timestamp_t t, std::size_t dataRate) const
 void Pin::serialize(std::ostream &stream) const {
     stream.write(reinterpret_cast<const char *>(&m_last_state), sizeof(m_last_state));
     stream.write(reinterpret_cast<const char *>(&m_last_set), sizeof(m_last_set));
-    pending_stats.serialize(stream);
+    m_pending_stats.serialize(stream);
     m_stats.serialize(stream);
 }
 
 void Pin::deserialize(std::istream &stream) {
     stream.read(reinterpret_cast<char *>(&m_last_state), sizeof(m_last_state));
     stream.read(reinterpret_cast<char *>(&m_last_set), sizeof(m_last_set));
-    pending_stats.deserialize(stream);
+    m_pending_stats.deserialize(stream);
     m_stats.deserialize(stream);
 }
 
