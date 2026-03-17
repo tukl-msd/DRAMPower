84e7836065b7b4387e908660414976af03b665cd
marcomoerz <64926170+marcomoerz@users.noreply.github.com>
2026-03-17 12:48:37 +0100

    refactor(tests): simplify test accessors

--- src/DRAMPower/DRAMPower/standards/test_accessor.h
+++ src/DRAMPower/DRAMPower/standards/test_accessor.h
@@ -1,38 +1,28 @@
 #ifndef DRAMPOWER_STANDARDS_TEST_INTERNAL_H
 #define DRAMPOWER_STANDARDS_TEST_INTERNAL_H
 
-#include "DRAMPower/standards/ddr4/DDR4.h"
-#include "DRAMPower/standards/ddr5/DDR5.h"
-#include "DRAMPower/standards/lpddr4/LPDDR4.h"
-#include "DRAMPower/standards/lpddr5/LPDDR5.h"
+#include "DRAMPower/standards/ddr4/DDR4Core.h"
+#include "DRAMPower/standards/ddr5/DDR5Core.h"
+#include "DRAMPower/standards/lpddr4/LPDDR4Core.h"
+#include "DRAMPower/standards/lpddr5/LPDDR5Core.h"
 
 namespace DRAMPower::internal {
 
 // Tests access to private members of DDR4, DDR5, LPDDR4, and LPDDR5 classes.
 // https://github.com/google/googletest/blob/main/docs/advanced.md#testing-private-code
-template<typename Standard, typename Core, typename Interface>
+template<typename Core>
 class TestAccessor {
 public:
-    static Core& getCore(Standard& standard) {
-        return standard.getCore();
-    }
-    static const Core& getCore(const Standard& standard) {
-        return standard.getCore();
-    }
-
-    static Interface& getInterface(Standard& standard) {
-        return standard.getInterface();
-    }
-    static const Interface& getInterface(const Standard& standard) {
-        return standard.getInterface();
+    static std::vector<Rank>& getRanks(Core& core) {
+        return core.m_ranks;
     }
 };
 
 // Specializations for each standard
-static const TestAccessor<DDR4, DDR4Core, DDR4Interface> DDR4TestAccessor;
-static const TestAccessor<DDR5, DDR5Core, DDR5Interface> DDR5TestAccessor;
-static const TestAccessor<LPDDR4, LPDDR4Core, LPDDR4Interface> LPDDR4TestAccessor;
-static const TestAccessor<LPDDR5, LPDDR5Core, LPDDR5Interface> LPDDR5TestAccessor;
+static const TestAccessor<DDR4Core> DDR4TestAccessor;
+static const TestAccessor<DDR5Core> DDR5TestAccessor;
+static const TestAccessor<LPDDR4Core> LPDDR4TestAccessor;
+static const TestAccessor<LPDDR5Core> LPDDR5TestAccessor;
 
 #ifndef DRAMPOWER_TESTING
 #error "test-internal.h should only be included in test files"
