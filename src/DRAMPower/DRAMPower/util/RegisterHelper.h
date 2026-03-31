d4a97bfee26ef6e8d4570cdce3c174e66fdab93e
marcomoerz <64926170+marcomoerz@users.noreply.github.com>
2026-03-31 18:11:49 +0200

    feat(registerHelper): added index middleWares

--- src/DRAMPower/DRAMPower/util/RegisterHelper.h
+++ src/DRAMPower/DRAMPower/util/RegisterHelper.h
@@ -22,12 +22,28 @@ namespace coreHelpers {
     }
 
     template<typename Func, typename Owner>
+    decltype(auto) bankHandlerIdx(const Command& cmd, std::vector<Rank>& ranks, Owner owner, Func &&member_func) {
+        assert(ranks.size()>cmd.targetCoordinate.rank);
+        assert(ranks.at(cmd.targetCoordinate.rank).banks.size()>cmd.targetCoordinate.bank);
+
+        ranks.at(cmd.targetCoordinate.rank).commandCounter.inc(cmd.type);
+        return (owner->*member_func)(cmd.targetCoordinate.rank, cmd.targetCoordinate.bank, cmd.timestamp);
+    }
+
+    template<typename Func, typename Owner>
     decltype(auto) rankHandler(const Command& cmd, std::vector<Rank>& ranks, Owner owner, Func &&member_func) {
         assert(ranks.size()>cmd.targetCoordinate.rank);
         auto & rank = ranks.at(cmd.targetCoordinate.rank);
 
         rank.commandCounter.inc(cmd.type);
         return (owner->*member_func)(rank, cmd.timestamp);
+    }
+
+    template<typename Func, typename Owner>
+    decltype(auto) rankHandlerIdx(const Command& cmd, std::vector<Rank>& ranks, Owner owner, Func &&member_func) {
+        assert(ranks.size()>cmd.targetCoordinate.rank);
+        ranks.at(cmd.targetCoordinate.rank).commandCounter.inc(cmd.type);
+        return (owner->*member_func)(cmd.targetCoordinate.rank, cmd.timestamp);
     }
 
     template<typename Func, typename Owner>
@@ -43,6 +59,21 @@ namespace coreHelpers {
 
         rank.commandCounter.inc(cmd.type);
         return (owner->*member_func)(rank, bank_id, cmd.timestamp);
+    }
+
+    template<typename Func, typename Owner>
+    decltype(auto) bankGroupHandlerIdx(const Command& cmd, std::vector<Rank>& ranks, Owner owner, Func &&member_func) {
+        assert(ranks.size()>cmd.targetCoordinate.rank);
+        auto& rank = ranks.at(cmd.targetCoordinate.rank);
+
+        assert(rank.banks.size()>cmd.targetCoordinate.bank);
+        if (cmd.targetCoordinate.bank >= rank.banks.size()) {
+            throw std::invalid_argument("Invalid bank targetcoordinate");
+        }
+        auto bank_id = cmd.targetCoordinate.bank;
+
+        rank.commandCounter.inc(cmd.type);
+        return (owner->*member_func)(cmd.targetCoordinate.rank, bank_id, cmd.timestamp);
     }
 } // namespace coreHelpers
 
