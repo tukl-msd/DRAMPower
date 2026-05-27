5fd7bf132a9379df7eb701817e65b804e23c4cf4
Derek Christ <derek.christ@uni-wuerzburg.de>
2026-05-27 12:56:13 +0200

    fix: resolve build issue with tests

--- src/DRAMPower/DRAMPower/simconfig/simconfig.h
+++ src/DRAMPower/DRAMPower/simconfig/simconfig.h
@@ -7,16 +7,8 @@
 namespace DRAMPower::config {
 
 struct SimConfig {
-// Public type definitions
-public:
     using ToggleRateDefinition_t = DRAMUtils::Config::ToggleRateDefinition;
 
-// Public Constructors
-public:
-    SimConfig() = default;
-
-// Public members
-public:
     std::optional<ToggleRateDefinition_t> toggleRateDefinition;
 };
 NLOHMANN_JSONIFY_ALL_THINGS(SimConfig, toggleRateDefinition);
